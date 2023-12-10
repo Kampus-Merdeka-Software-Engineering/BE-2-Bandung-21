2e2a4164127de3405d7cb0736ff97450b5edbf88
Adzra Labiibah Aathifah <adzralabiibah03@gmail.com>
2023-12-11 00:27:56 +0700

    update

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const db = mysql.createPool({
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE,
+    host:process.env.host,
+    port:process.env.port,
+    user:process.env.user,
+    password:process.env.password,
+    database:process.env.database,
 
 })
 
